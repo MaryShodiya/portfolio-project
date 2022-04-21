0d148c4abd2795e28f941803691d56f71a8aa485
MaryShodiya <marysho57@gmail.com>
2022-04-21 19:39:49 +0100

    added resume doc

--- js/app.js
+++ js/app.js
@@ -9,9 +9,21 @@ function slideMenu() {
     navigation.classList.toggle('toggle_navigation');
 }
 
-/*Anaimate on scroll config */
+/*Animate on scroll config */
 AOS.init(
     {
         delay: 50,
     }
 );
+button.getElementById('resume').addEventListener('click', myResume)
+function myResume(){
+var iframe;
+
+iframe = document.createElement('iframe')
+iframe.src = 'https://onedrive.live.com/embed?cid=6250224E2DA231F9&amp;resid=6250224E2DA231F9%21310&amp;authkey=AA3QeUA3o52a-SI&amp;em=2'
+iframe.style.display = 'block'
+iframe.style.width= '100%';
+iframe.style.height = '556px';
+iframe.style.frameBorder = '0'
+document.body.appendChild(iframe)
+}
